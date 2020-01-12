c06e1e0d75800674cfe6d28d144bb7ef20a4737b
cypridina <darrin.schultz91@gmail.com>
2020-01-12 10:43:28 +0300

    updating transcript classes

--- annotation/horm_annotation_script.py
+++ annotation/horm_annotation_script.py
@@ -3,101 +3,29 @@ import pandas as pd
 import numpy as np
 import gzip
 import collections
-
-workingdir="/Users/darrin/git/hormiphora/annotation/"
-filepath = "{}/raw_files/Hcal_annotation_v5.csv".format(workingdir)
-
-df = pd.read_csv(filepath, header=0, sep=',')
-df["checked"] = "none"
-
-#First make sure that someone has checked the gene
-for i, row in df.iterrows():
-    C1 = False
-    C2 = False
-    if type(row["DTS_checked"]) == str:
-        C1 = row["DTS_checked"].strip().lower() in ['y', 'yes']
-    if type(row["WRF_checked"]) == str:
-        C2 = row["WRF_checked"].strip().lower() in ['y', 'yes']
-    df.at[i,'checked'] = C1 or C2
-
-# First make sure that each row has one of DTS_checked or WRF_checked
-t1 = df.loc[df['checked'] == False, ]
-assert len(t1) == 0
-
-# Now make sure that each row has something in stringtie_id, isoseq_hq_id, pinfish_id, or isoseq_singleton_id
-#  don't count rows that also have isoseq reads containing ID m64069
-df["one_row_one_gene"] = "none"
-for i, row in df.iterrows():
-    C1_ST = False
-    C2_IS = False
-    C3_PF = False
-    C4_SI = False
-    C5_CO = False
-    if type(row["stringtie_id"]) == str:
-        C1_ST = row["stringtie_id"].strip().lower() != ""
-    if type(row["isoseq_hq_id"]) == str:
-        C2_IS = row["isoseq_hq_id"].strip().lower() != ""
-    if type(row["pinfish_id"]) == str:
-        C3_PF = row["pinfish_id"].strip().lower() != ""
-    if type(row["isoseq_singleton_id"]) == str:
-        C4_SI = row["isoseq_singleton_id"].strip().lower() != ""
-    if type(row["comment"]) == str:
-        if 'm64069' in row["comment"].strip().lower():
-            C5_CO = True
-    df.at[i,'one_row_one_gene'] = C1_ST or C2_IS or C3_PF or C4_SI or C5_CO
-
-t1 = df.loc[df['one_row_one_gene'] == False, ]
-assert len(t1) == 0
-
-# Now get a list of genes left over that still need annotation from reads
-#  m64069
-df["only_minimap"] = "none"
-for i, row in df.iterrows():
-    C1_ST = False
-    C2_IS = False
-    C3_PF = False
-    C4_SI = False
-    C5_CO = False
-    if type(row["stringtie_id"]) == str:
-        C1_ST = row["stringtie_id"].strip().lower() != ""
-    if type(row["isoseq_hq_id"]) == str:
-        C2_IS = row["isoseq_hq_id"].strip().lower() != ""
-    if type(row["pinfish_id"]) == str:
-        C3_PF = row["pinfish_id"].strip().lower() != ""
-    if type(row["isoseq_singleton_id"]) == str:
-        C4_SI = row["isoseq_singleton_id"].strip().lower() != ""
-    df.at[i,'only_minimap'] = C1_ST or C2_IS or C3_PF or C4_SI
-
-t1 = df.loc[df['only_minimap'] == False, ]
-print(t1)
-# just skip this for now since it's in-progress
-#assert len(t1) == 0
-
-# Now we make sure that all of the pinfish files have unique IDs
-#  if they are a hash, all the IDs will be unique
-P1="/Users/darrin/Downloads/HC_spliced/UCSC_Hcal_v1_B1_LR.pinfish_clusters.gff.gz"
-P2="/Users/darrin/Downloads/HC_spliced/UCSC_Hcal_v1_B1_LR.pinfish_clusters_c7p10.gff.gz"
-P3="/Users/darrin/Downloads/HC_spliced/UCSC_Hcal_v1_B1_LR.pinfish_clusters_c2p20.gff.gz"
-
-ids = {}
-Files = [P1, P2, P3]
-for thisfile in Files:
-    with gzip.open(thisfile, "rb") as this_f:
-        for line in this_f:
-            line = line.decode("utf-8")
-            if "pinfish	transcript" in line:
-                print(line.split('\t'))
-                sys.exit()
-                ID = line.split('\t')[8].split(';')[0].replace("ID=","")
-                if ID in ids:
-                    ids[ID] += 1
-                else:
-                    ids[ID] = 1
-assert len(np.unique(list(ids.values()))) == 1
-#yes, all the hashes are the same
+import transcript_classes as tc
 
 def main():
-    pass
+    annotation_spreadsheet = "raw_files/Hcal_annotation_v5.csv"
+    pinfish_files = ["raw_files/UCSC_Hcal_v1_B1_LR.pinfish_clusters.gff.gz",
+                     "raw_files/UCSC_Hcal_v1_B1_LR.pinfish_clusters_c7p10.gff.gz",
+                     "raw_files/UCSC_Hcal_v1_B1_LR.pinfish_clusters_c2p20.gff.gz"]
+    # Now we make sure that all of the pinfish files have unique IDs
+    #  if they are a hash, all the IDs will be unique
+
+    # read in the spreadsheet
+    df = pd.read_csv(annotation_spreadsheet, header=0, sep=',')
+    #First make sure that someone has checked the gene
+    df = tc.sumone_has_checked(df)
+    # now make sure that each row has something (a gene/transcript)
+    df = tc.each_row_has_something(df)
+    # now make sure that there are no more genes that still need a transcript,
+    #  but that have a minimap ID
+    tc.still_needs_transcript(df)
+    # make a dict to store the GFF files
+    GFFs = {}
+    GFFs["pinfish"] = tc.gffFile(pinfish_files, "pinfish")
+    print(GFFs["pinfish"].IDTS)
 
 if __name__== "__main__":
     main()
--- annotation/transcript_classes.py
+++ annotation/transcript_classes.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+import gzip
+
 class Gene:
     """
     just a container for holding information about genes.
     mostly useful to hold information to add to the flags,
     like "interesting", "operon", et cetera
     """
-    
+
 class Transcript:
     """
     Instance attributes:
@@ -16,7 +18,7 @@ class Transcript:
         - The scaffold is the name of the scaffold on which the transcript is
           located. Like 'c1', 'c2', 'M', et cetera
      - num_on_scaffold
-        - This is the index of the transcript on that scaffold. For instance 
+        - This is the index of the transcript on that scaffold. For instance
           if this gene is the 117th gene that occurs when looking at the chromosome
           from 5' to 3', then this number is 117
      - ID
@@ -24,7 +26,7 @@ class Transcript:
           provided in filename. Depending on the filetype this will change how things
           are parsed
      - ftype
-        - The program that was used to generate the transcripts. Can be one of 
+        - The program that was used to generate the transcripts. Can be one of
           the following types.
           - "stringtie"
           - "manual"
@@ -56,8 +58,8 @@ class gffFile:
     This is just a way to keep track of what transcripts are in what file.
     It will contain all of the necessary information to pull out whole genes
      or individual transcripts from a gff file.
-    
-     - The program that was used to generate the transcripts. Can be one of 
+
+     - The program that was used to generate the transcripts. Can be one of
         the following types.
         - "stringtie"
         - "manual"
@@ -73,8 +75,11 @@ class gffFile:
 
         #IDTS = transcript ID To String
         self.IDTS = {}
+        # GTT genes to transcripts
+        self.GTT  = {}
         self.filename = filename
-        if type(filename) == list and filetype == pinfish:
+        self.filetype = filetype
+        if type(filename) == list and self.filetype == "pinfish":
             for thisfile in self.filename:
                 isgz = False
                 if thisfile.endswith(".gff.gz"):
@@ -83,7 +88,8 @@ class gffFile:
                 elif thisfile.endswith(".gff"):
                     f = open(thisfile, "r")
                 else:
-                    raise Error("""dunno what kind of file this is.
+                    print("bad filename -> ", filename)
+                    raise Exception("""dunno what kind of file this is.
                                 must be .gff or .gff.gz""")
                 for line in f:
                     nl = ""
@@ -91,39 +97,197 @@ class gffFile:
                         nl = line.decode("utf-8")
                     else:
                         nl=line
-                    splitd = line.split('\t')
-                    if splitd[1] == "pinfish" and splitd[2] == "transcript":
-                        # we have just found a new transcript in pinfish
-                        if not splitd[8].split(';')[0].startswith("ID="):
-                            #make sure the input is legal
-                            # should start with ID=
-                            print(line)
-                            raise Error("""There is some input error. We found
-                            a line that doesn't have field 9 starting with ID=.
-                            all pinfish transcripts start with this""")
-                        ID = splitd[8].split(';')[0].replace("ID=","")
-                        if ID in self.IDTS:
-                            raise Error("""This transcript is already in the map.
+                    splitd = nl.split('\t')
+                    if splitd[2] == "transcript":
+                        # we have just found a new transcript.
+                        # make sure that the input is legal
+                        if splitd[1] in  ["pinfish", "StringTie"]:
+                            if not splitd[8].split(';')[0].startswith("ID="):
+                                # should start with ID=
+                                print(line)
+                                raise Exception("""There is some input error. We found
+                                a line that doesn't have field 9 starting with ID=.
+                                all pinfish/StringTie transcripts start with this""")
+                        elif splitd[1] in ["PacBio"]:
+                            if not splitd[8].split(';')[0].startswith("gene_id"):
+                                # should start with gene_id
+                                print(line)
+                                raise Exception("""There is some input error. We found
+                                a line that doesn't have field 9 starting with gene_id.
+                                all PacBio transcripts start with this""")
+
+                        # now that we made sure the input is legal, let's parse
+                        #  the gene id and the transcript ID.
+                        if splitd[1] == "pinfish":
+                            tID = splitd[8].split(';')[0].replace("ID=","").strip()
+                            gID = tID
+                        elif splitd[1] == "StringTie":
+                            tID = splitd[8].split(';')[0].replace("ID=","").strip()
+                            gID = ".".join(tID.split('.')[0:-1])
+                        elif splitd[1] == "PacBio":
+                            tID = splitd[8].split(';')[1].replace("transcript_id","").strip().replace("\"", "")
+                            gID = splitd[8].split(';')[0].replace("gene_id","").strip().replace("\"", "")
+                        if tID in self.IDTS:
+                            raise Exception("""This transcript is already in the map.
                                         we shouldn't see it here yet.""")
-                        #just add the line to the dict. we'll parse it later
-                        self.IDTS[ID] = nl
-                    elif splitd[1] == "pinfish" and splitd[2] == "exon":
+                        # Now that we have the transcript ID and the geneID
+                        #  store them in the instance
+                        if splitd[1] == "pinfish":
+                            # every pinfish transcript is also its own gene.
+                            #  each transcript just has its own hash.
+                            assert tID not in self.IDTS
+                            assert gID not in self.GTT
+                            self.GTT[gID] = [tID]
+                        elif splitd[1] in ["StringTie", "PacBio"]:
+                            assert tID not in self.IDTS
+                            if gID not in self.GTT:
+                                self.GTT[gID] = [tID]
+                            else:
+                                self.GTT[gID].append(tID)
+                        self.IDTS[tID] = nl
+                    elif splitd[2] == "exon":
                         # we have found an exon for this transcript. We should
                         #  have already found the transcript itself.
-                        if not splitd[8].split(';')[0].startswith("Parent="):
-                            #make sure the input is legal
-                            # should start with Parent=
-                            print(line)
-                            raise Error("""There is some input error. We found
-                          a line that doesn't have field 9 starting with Parent=.
-                            all pinfish exons start with this""")
-                        ID = splitd[8].split(';')[0].replace("Parent=","")
-                        if ID not in self.IDTS:
+
+                        #make sure that the input is legal
+                        if splitd[1] in  ["pinfish"]:
+                            if not splitd[8].split(';')[0].startswith("Parent="):
+                                print(line)
+                                raise Exception("""There is some input error. We found
+                              a line that doesn't have field 9 starting with Parent=.
+                                all pinfish exons start with this""")
+                        elif splitd[1] in  ["StringTie"]:
+                            if not splitd[8].split(';')[0].startswith("ID="):
+                                print(line)
+                                raise Exception("""There is some input error. We found
+                              a line that doesn't have field 9 starting with ID=.
+                                all pinfish exons start with this""")
+                        elif splitd[1] in  ["PacBio"]:
+                            if not splitd[8].split(';')[0].startswith("gene_id \""):
+                                print(line)
+                                raise Exception("""There is some input error. We found
+                              a line that doesn't have field 9 starting with ID=.
+                                all pinfish exons start with this""")
+
+
+                        # now that we made sure the input is legal, let's parse
+                        #  the gene id and the transcript ID.
+                        if splitd[1] == "pinfish":
+                            tID = splitd[8].split(';')[0].replace("Parent=","").strip()
+                        elif splitd[1] == "StringTie":
+                            tID = splitd[8].split(';')[1].replace("Parent=","").strip()
+                        elif splitd[1] == "PacBio":
+                            tID = splitd[8].split(';')[1].replace("transcript_id","").strip().replace("\"", "")
+
+                        # Now that we have the transcript ID
+                        #  store them in the instance
+                        if tID not in self.IDTS:
                             # we should have already seen the transcript
                             #  if the gff file is sorted properly
-                            raise Error("For some reason we found an exon for a
+                            print("offending ID: ", tID)
+                            print("offending file:", thisfile)
+                            raise Exception("""For some reason we found an exon for a
                             transcript before we found the transcript itself.
-                            The GFF file should have all of the transcripts first.")
-                        self.IDTS[ID] = nl
+                            The GFF file should have all of the transcripts first.""")
+                        self.IDTS[tID] += nl
 
+def sumone_has_checked(df):
+    df["checked"] = "none"
+    #First make sure that someone has checked the gene
+    for i, row in df.iterrows():
+        C1 = False
+        C2 = False
+        if type(row["DTS_checked"]) == str:
+            C1 = row["DTS_checked"].strip().lower() in ['y', 'yes']
+        if type(row["WRF_checked"]) == str:
+            C2 = row["WRF_checked"].strip().lower() in ['y', 'yes']
+        df.at[i,'checked'] = C1 or C2
+    t1 = df.loc[df['checked'] == False, ]
+    # program will crash if this fails
+    assert len(t1) == 0
 
+    return(df)
+
+def each_row_has_something(df):
+    # Now make sure that each row has something in stringtie_id,
+    #  isoseq_hq_id, pinfish_id, or isoseq_singleton_id
+    # don't count rows that also have isoseq reads containing ID m64069
+    df["one_row_one_gene"] = "none"
+    for i, row in df.iterrows():
+        C1_ST = False
+        C2_IS = False
+        C3_PF = False
+        C4_SI = False
+        C5_CO = False
+        if type(row["stringtie_id"]) == str:
+            C1_ST = row["stringtie_id"].strip().lower() != ""
+        if type(row["isoseq_hq_id"]) == str:
+            C2_IS = row["isoseq_hq_id"].strip().lower() != ""
+        if type(row["pinfish_id"]) == str:
+            C3_PF = row["pinfish_id"].strip().lower() != ""
+        if type(row["isoseq_singleton_id"]) == str:
+            C4_SI = row["isoseq_singleton_id"].strip().lower() != ""
+        if type(row["comment"]) == str:
+            if 'm64069' in row["comment"].strip().lower():
+                C5_CO = True
+        df.at[i,'one_row_one_gene'] = C1_ST or C2_IS or C3_PF or C4_SI or C5_CO
+
+    t1 = df.loc[df['one_row_one_gene'] == False, ]
+    assert len(t1) == 0
+    return(df)
+
+# print out list of genes that still need a transcript
+def still_needs_transcript(df):
+    # Now get a list of genes left over that still need annotation from reads
+    #  m64069
+    df["only_minimap"] = "none"
+    for i, row in df.iterrows():
+        C1_ST = False
+        C2_IS = False
+        C3_PF = False
+        C4_SI = False
+        C5_CO = False
+        if type(row["stringtie_id"]) == str:
+            C1_ST = row["stringtie_id"].strip().lower() != ""
+        if type(row["isoseq_hq_id"]) == str:
+            C2_IS = row["isoseq_hq_id"].strip().lower() != ""
+        if type(row["pinfish_id"]) == str:
+            C3_PF = row["pinfish_id"].strip().lower() != ""
+        if type(row["isoseq_singleton_id"]) == str:
+            C4_SI = row["isoseq_singleton_id"].strip().lower() != ""
+        df.at[i,'only_minimap'] = C1_ST or C2_IS or C3_PF or C4_SI
+
+    t1 = df.loc[df['only_minimap'] == False, ]
+    print(t1)
+    # just skip this for now since it's in-progress
+    #assert len(t1) == 0
+    return(df)
+
+# now make sure that each pinfish file has a unique hash
+def check_pinfish_ids(pinfish_list):
+    ids = {}
+    for thisfile in pinfish_list:
+        if thisfile.endswith(".gff.gz"):
+            f = gzip.open(thisfile, "rb")
+            isgz = True
+        elif thisfile.endswith(".gff"):
+            f = open(thisfile, "r")
+        else:
+            raise Exception("""dunno what kind of file this is.
+                        must be .gff or .gff.gz""")
+        for line in f:
+            nl = ""
+            if isgz:
+                nl = line.decode("utf-8")
+            else:
+                nl=line
+            if "transcript" in nl and "pinfish" in nl:
+                print(line.split('\t'))
+                sys.exit()
+                ID = line.split('\t')[8].split(';')[0].replace("ID=","")
+                if ID in ids:
+                    ids[ID] += 1
+                else:
+                    ids[ID] = 1
+    assert len(np.unique(list(ids.values()))) == 1
+    #yes, all the hashes are the same
